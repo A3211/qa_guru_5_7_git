d3dd0863df7bbdc59aefb6b6ffb7cf64b3dc2e8b
A3211 <a.kusl1122@gmail.com>
2021-04-05 21:42:23 +0700

    Общий комит

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -1,7 +1,10 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTest {
@@ -9,5 +12,9 @@ public class SearchTest {
     @Test
     void googleSearchTest() {
         open("https://www.google.com");
+        $(By.name("q")).setValue("Selenide").pressEnter();
+        $("body").shouldHave(Condition.text("Selenide: лаконичные и стабильные UI тесты на Java"));
+        //2 коммит
+        //3 коммит
     }
 }
